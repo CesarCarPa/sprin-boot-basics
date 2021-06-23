84495a00c34671956b1ec006a802ac4c61e67f5e
Cesar Cardona <ceaucarpa88@gmail.com>
2021-06-23 17:48:08 -0500

    progress

--- src/main/java/com/cacp/learning/lesson_01/entity/User.java
+++ src/main/java/com/cacp/learning/lesson_01/entity/User.java
@@ -1,18 +1,34 @@
 package com.cacp.learning.lesson_01.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
+@Entity
 public class User {
 
-    private int id;
+	@Id
+	@GeneratedValue
+    private Integer id;
+	
     @Size(min = 2, message = "Must contains at least 2 characters.")
     private String name;
+    
     @Past
     private Date birthDate;
+    
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
-    public User(int id, String name, Date birthDate) {
+    public User() {
+	}
+
+	public User(int id, String name, Date birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
@@ -22,7 +38,7 @@ public class User {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -42,7 +58,15 @@ public class User {
         this.birthDate = birthDate;
     }
 
-    @Override
+    public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	@Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
